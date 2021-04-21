da9df1159b0e7e11461f4b6c960820864080ef12
Ayse Cimen Basar <aysebasar91@gmail.com>
2021-04-21 19:18:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ main.remove();
 const newHeader = document.createElement("h1");
 newHeader.setAttribute("id", "victory");
 document.body.appendChild(newHeader)
-newHeader.innerText = "Ayse is the champion"
+newHeader.innerText = "YOUR-NAME is the champion"
 console.log(newHeader)
